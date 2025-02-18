8fbc2c628b2d204f7b5b771e99fcce5345dc5347
tyloo <jbonva@gmail.com>
2025-02-18 13:34:53 +0100

    Removed useless Sonner
    Signed-off-by: tyloo <jbonva@gmail.com>

--- contexts/providers.tsx
+++ contexts/providers.tsx
@@ -1,20 +1,12 @@
 'use client'
 
-import { Toaster } from '@/components/ui/sonner'
-import { ThemeProvider, useTheme } from 'next-themes'
+import { ThemeProvider } from 'next-themes'
 import { SidebarProvider } from './sidebar-context'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider enableSystem attribute='class' defaultTheme='system' disableTransitionOnChange>
       <SidebarProvider>{children}</SidebarProvider>
-      <ToasterProvider />
     </ThemeProvider>
   )
-}
-
-function ToasterProvider() {
-  const { resolvedTheme } = useTheme()
-
-  return <Toaster position='top-right' theme={resolvedTheme === 'dark' ? 'dark' : 'light'} />
 }
